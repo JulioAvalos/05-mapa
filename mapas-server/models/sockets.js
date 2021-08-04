080e8387de1d4fce3d28d94c9c4b71ad3124c9ab
JulioAvalos <julioavalos2@msn.com>
2021-08-03 20:08:25 -0600

    adding marker and marker list models

--- mapas-server/models/sockets.js
+++ mapas-server/models/sockets.js
@@ -1,3 +1,4 @@
+const Marcadores = require("./marcadores");
 
 
 class Sockets {
@@ -6,21 +7,21 @@ class Sockets {
 
         this.io = io;
 
+        this.marcadores = new Marcadores();
+
         this.socketEvents();
     }
 
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
             
-        
+            // TODO: marcadores-activos
+
+            // TODO: marcador-nuevo
+
+            // TODO: marcador-actualizado
+
         });
     }
 
